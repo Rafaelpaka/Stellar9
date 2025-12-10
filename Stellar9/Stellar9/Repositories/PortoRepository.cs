cdc201accf3378baee328c3124f67611f7fd69b8
Rafaelpaka <rafael24un@gmail.com>
2025-12-10 08:01:35 -0300

    Add files via upload

---
+++ Stellar9/Stellar9/Repositories/PortoRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using Stellar9.modelos;
+namespace Stellar9.Repositories
+{
+    public class PortoRepository
+    {
+        private readonly List<Nave> naves = new List<Nave>();
+        private readonly List<Viagem> viagens = new List<Viagem>();
+
+
+    }
+}
